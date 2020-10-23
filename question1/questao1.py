a3a10c0164b0660a5c9a2872fc56778c6038ddba
iderval <idervalsama@gmail.com>
2020-10-22 23:59:22 -0300

    Validação do modelo

--- question1/questao1.py
+++ question1/questao1.py
@@ -4,20 +4,6 @@ from tensorflow import keras as keras
 import matplotlib.pyplot as plt
 import csv
 
-# def switch_demo(argument):
-#     switcher = {
-#         0: [1, -1, -1, -1, -1, -1, -1, -1],
-#         1: [1, 1, -1, -1, -1, -1, -1, -1],
-#         2: [1, -1, 1, -1, -1, -1, -1, -1],
-#         3: [1, -1, -1, 1, -1, -1, -1, -1],
-#         4: [1, -1, -1, -1, 1, -1, -1, -1],
-#         5: [1, -1, -1, -1, -1, 1, -1, -1],
-#         6: [1, -1, -1, -1, -1, -1, 1, -1],
-#         7: [1, -1, -1, -1, -1, -1, -1, 1]
-#     }
-#     return switcher.get(argument)
-
-
 x_train = []
 y_train = []
 #opening csv
@@ -25,33 +11,29 @@ with open('dados.csv',newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     line = 0
     for row in reader:
-        if line != 0:
+        if line > 1:
             xrow = [float(row['c1']),float(row['c2']),float(row['c3'])]
             y_row = int(row['y'])
             x_train.append(xrow)
             y_train.append(y_row)
         line += 1
 
+data_x = np.asarray(x_train)
+data_y = np.asarray(y_train)
 
-##mnist = keras.datasets.mnist
-
-## x_train = [[0,0],[0,1],[1,0],[1,1]]
-# y_train = [0,1,2,3]
-x_train = np.asarray(x_train)
-y_train = np.asarray(y_train)
-x_train = keras.utils.normalize(x_train, axis=1)
-# # # x_test = keras.utils.normalize(x_test, axis=1)
+x_train, x_val = np.split(data_x, 2)
+y_train, y_val = np.split(data_y, 2)
 
 model = keras.models.Sequential()
 
 model.add(keras.layers.Flatten())
-# # model.add(keras.layers.Dense(128, activation=tf.nn.relu))
-# # model.add(keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(keras.layers.Dense(8, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy','mse'])
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=30)
+model.fit(x_train, y_train, epochs=25)
 
-# val_loss, val_acc = model.evaluate(x_test, y_test)
-# print(val_loss, val_acc)
+validation_arr = model.evaluate(x_val, y_val)
+print("Validation loss: " + str(validation_arr[0]) + "\nValidation accuracy: " + str(validation_arr[1]))
+
+
